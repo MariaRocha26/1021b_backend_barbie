555a87b02b432c635669aba96bec655eb66e5c6f
MariaRocha26 <maria.rocha6@estudante.ifms.edu.br>
2023-10-26 10:17:24 -0400

    aula dia 26

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,41 @@
 import express from 'express'
 const app = express()
 app.use(express.json())
-//tenho que ter uma rota post para cadastrar um filme
-app.post('/filmes', (req, res)=>{
+//Tenho que ter uma rota post para cadastrar um filme
+
+//Salvar em algum lugar o filme que foi cadastrado.
+type Filme = {
+    id:number,
+    titulo:string,
+    descricao:string,
+    imagem:string
+}
+let filmesCadastros:Filme[] = []
+app.post('/filmes',(req,res)=>{
+    const {id,titulo,descricao,imagem} = req.body
     const filme = {
-        titulo:'A escola do bem e do mal',
-        descricao:'História de duas amigas em um mundo mágico.',
-        imagem:"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/a_escola_do_bem_e_do_mal-26094185.jpg"
+        id,
+        titulo,
+        descricao,
+        imagem
     }
+    //Como eu salvo o filme que foi cadastrado no meu vetor de filmes (Banco de dados)
+    filmesCadastros.push(filme)
     res.status(201).send(filme)
 })
-app.get('/filmes', (req, res)=>{
-    res.send("Filme listado com sucesso!")
+app.get('/filmes',(req,res)=>{
+    res.send("Filmes Listados com sucesso")
 })
-//tenho que iniciar
+
+app.get('/filmes/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    //FIND para buscar um filme pelo id e retornar para o usuário
+    const filme = filmesCadastros.find(filme=>filme.id === id)
+    if(!filme) return res.status(404).send('Filme não encontrado')
+    res.status(200).send(filme)
+})
+
+//Tenho que iniciar o servidor na porta 3000
 app.listen(3000,()=>{
-    console.log("Servidor rodando na porta 3000")
+    console.log('Servidor rodando na porta 3000')
 })
