e0f34b66759dc4de814143b6bcb34cc77f78bad9
MariaRocha26 <maria.rocha6@estudante.ifms.edu.br>
2023-11-09 10:13:20 -0400

    atualizando banco

--- src/infra/banco/banco-em-memoria.test.ts
+++ src/infra/banco/banco-em-memoria.test.ts
@@ -1,10 +1,10 @@
-import { describe, expect, it, test} from 'vitest'
+import {describe, expect, it, test} from 'vitest'
 
 import BancoEmMemoria from './banco-em-memoria'
 
 describe('BancoEmMemoria', () => {
-    test('Deve salvar um filme em memoria',async () => {
-        //Entrada
+    test('deve salvar um filme em memória', async () => {
+        //Entrada de dados
         const filme = {
             id:1,
             titulo:'Filme 1',
@@ -14,11 +14,12 @@ describe('BancoEmMemoria', () => {
         //Processamento
         const bancoEmMemoria = new BancoEmMemoria()
         await bancoEmMemoria.salvar(filme)
+
         //Saída
         expect(bancoEmMemoria.filmes).toEqual([filme])
         expect(bancoEmMemoria.filmes.length).toBe(1)
     })
-    test('deve listar os filmes salvos em memoria',async () => {
+    test('deve listar os filmes salvos em memória',async () => {
         //Entrada
         const filme = {
             id:1,
@@ -26,15 +27,15 @@ describe('BancoEmMemoria', () => {
             descricao:'Descrição do filme 1',
             imagem:'imagem.jpg'
         }
-        //Processamento
+        //processamento
         const bancoEmMemoria = new BancoEmMemoria()
         bancoEmMemoria.filmes.push(filme)
         const resultado = await bancoEmMemoria.listar()
-        //Saída
+        //saída
         expect(resultado).toEqual([filme])
     })
-    test('deve buscar um filme pelo id',async () => {
-        //Entrada
+    test('deve buscar um filme pelo id', async() => {
+        //entrada
         const filme1 = {
             id:1,
             titulo:'Filme 1',
@@ -47,13 +48,14 @@ describe('BancoEmMemoria', () => {
             descricao:'Descrição do filme 1',
             imagem:'imagem.jpg'
         }
-        //Processamento
+        //processamento
         const bancoEmMemoria = new BancoEmMemoria()
         bancoEmMemoria.filmes.push(filme1)
         bancoEmMemoria.filmes.push(filme2)
-        
+
         const resultado = await bancoEmMemoria.buscarPorId(1)
-        //Saída
+
+        //saída
         expect(resultado).toEqual(filme1)
         expect(bancoEmMemoria.filmes).toHaveLength(2)
     })
