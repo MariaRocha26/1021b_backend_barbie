1bea5db9ed8e37247e7e2880a03a25286212ad8a
MariaRocha26 <maria.rocha6@estudante.ifms.edu.br>
2023-11-16 10:02:49 -0400

    mexendo com thunder client

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 import ListaFilme from './aplicacao/lista-filme.use-case'
+import SalvaFilme from './aplicacao/salva-filme.use-case'
 import BancoMongoDB from './infra/banco/banco-mongodb'
-//Tenho que ter uma rota post para cadastrar um filme
 
-//Salvar em algum lugar o filme que foi cadastrado.
+const bancoMongoDB = new BancoMongoDB()
+
 type Filme = {
     id:number,
     titulo:string,
@@ -13,7 +14,7 @@ type Filme = {
     imagem:string
 }
 let filmesCadastros:Filme[] = []
-app.post('/filmes',(req,res)=>{
+app.post('/filmes', async(req,res)=>{
     const {id,titulo,descricao,imagem} = req.body
     const filme = {
         id,
@@ -21,12 +22,11 @@ app.post('/filmes',(req,res)=>{
         descricao,
         imagem
     }
-    //Como eu salvo o filme que foi cadastrado no meu vetor de filmes (Banco de dados)
-    filmesCadastros.push(filme)
-    res.status(201).send(filme)
+    const novoFilme = new SalvaFilme(bancoMongoDB)
+    const filmes = await novoFilme.execute(filme)
+    res.status(201).send(filmes)
 })
 app.get('/filmes', async (req,res)=>{
-    const bancoMongoDB = new BancoMongoDB()
     const listaFilme = new ListaFilme(bancoMongoDB)
     const filmes = await listaFilme.execute()   
     res.send(filmes)
@@ -34,13 +34,12 @@ app.get('/filmes', async (req,res)=>{
 
 app.get('/filmes/:id',(req,res)=>{
     const id = parseInt(req.params.id)
-    //FIND para buscar um filme pelo id e retornar para o usuário
     const filme = filmesCadastros.find(filme=>filme.id === id)
     if(!filme) return res.status(404).send('Filme não encontrado')
     res.status(200).send(filme)
 })
 
-//Tenho que iniciar o servidor na porta 3000
+
 app.listen(3000,()=>{
     console.log('Servidor rodando na porta 3000')
 })
